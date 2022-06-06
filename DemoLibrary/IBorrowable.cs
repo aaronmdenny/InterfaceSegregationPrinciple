d5568ef995226ae2cb976b2e742e108730ef0b60
aaronmdenny <aaronmdenny@outlook.com>
2022-06-06 09:35:04 -0400

    fill in IBorrowable

--- DemoLibrary/IBorrowable.cs
+++ DemoLibrary/IBorrowable.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DemoLibrary
 {
-    internal interface IBorrowable
+    public interface IBorrowable
     {
+        DateTime BorrowDate { get; set; }
+
+        string Borrower { get; set; }
+
+        int CheckOutDurationInDays { get; set; }
+
+        void CheckIn();
+
+        void CheckOut(string borrower);
+
+        DateTime GetDueDate();
     }
 }
