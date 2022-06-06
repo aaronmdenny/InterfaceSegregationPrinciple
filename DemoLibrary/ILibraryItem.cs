0a12469896d591637402844619a6afc34cf1dd1c
aaronmdenny <aaronmdenny@outlook.com>
2022-06-06 09:31:15 -0400

    IBorrowable interface

--- DemoLibrary/ILibraryItem.cs
+++ DemoLibrary/ILibraryItem.cs
@@ -1,23 +1,9 @@
-﻿using System;
-
-namespace DemoLibrary
+﻿namespace DemoLibrary
 {
     public interface ILibraryItem
     {
         string LibraryId { get; set; }
 
         string Title { get; set; }
-
-        //DateTime BorrowDate { get; set; }
-
-        //string Borrower { get; set; }
-
-        //int CheckOutDurationInDays { get; set; }
-
-        //void CheckIn();
-
-        //void CheckOut(string borrower);
-
-        //DateTime GetDueDate();
     }
 }
