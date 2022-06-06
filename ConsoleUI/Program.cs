902f19afe4cca449df2f8b64fc4b0e4a7d0ec9c4
aaronmdenny <aaronmdenny@outlook.com>
2022-06-06 11:49:26 -0400

    demonstrate usefulness of implementing one interface

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLibrary;
+using System;
 
 namespace ConsoleUI
 {
@@ -10,7 +11,12 @@ namespace ConsoleUI
              * Interface Segregation Principle: A client should not be forced to depend upon interfaces it does not use.
              * 
              * We will break down ILibraryItem into smaller pieces.
+             * 
+             * We created the empty IBorrowableDVD interface to allow access to all properties/methods in IDVD and
+             * IBorrowable. 
              */
+            IBorrowableDVD dvd = new DVD();
+
             Console.ReadLine();
         }
     }
