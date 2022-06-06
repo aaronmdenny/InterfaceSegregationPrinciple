1df69d14c85a5e2ce5021fcc46f68b01f7408eba
aaronmdenny <aaronmdenny@outlook.com>
2022-06-06 09:25:31 -0400

    comment out ILibraryItem props that do not apply

--- DemoLibrary/ILibraryItem.cs
+++ DemoLibrary/ILibraryItem.cs
@@ -4,24 +4,24 @@ namespace DemoLibrary
 {
     public interface ILibraryItem
     {
-        string Author { get; set; }
-
-        DateTime BorrowDate { get; set; }
-
-        string Borrower { get; set; }
-
-        int CheckOutDurationInDays { get; set; }
-
         string LibraryId { get; set; }
-
-        int Pages { get; set; }
 
         string Title { get; set; }
 
-        void CheckIn();
+        //string Author { get; set; }
 
-        void CheckOut(string borrower);
+        //DateTime BorrowDate { get; set; }
 
-        DateTime GetDueDate();
+        //string Borrower { get; set; }
+
+        //int CheckOutDurationInDays { get; set; }
+
+        //int Pages { get; set; }
+
+        //void CheckIn();
+
+        //void CheckOut(string borrower);
+
+        //DateTime GetDueDate();
     }
 }
